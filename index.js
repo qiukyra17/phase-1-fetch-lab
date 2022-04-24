be7e434af0e2afc8feab02caf140b6975b09f802
kyra qiu <kyraqiu@gmail.com>
2022-04-23 21:47:12 -0400

    commit tree 09f2e3c41ac002bf46337232d4e94ce313ad1012

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch ('https://anapioficeandfire.com/api/books')
+ .then(res => res.json())
+ .then(data => renderBooks(data));
+  // .then(function (response) {
+  //   return response.json();
+  // })
+  // .then(function (data) {
+  //   return renderBooks(data);
+  // });
 }
 
 function renderBooks(books) {
@@ -11,6 +19,7 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
